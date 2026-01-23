c27e6ee1254239cc5e9bfd0177c55e591ec43eee
Sleepy <158308669+SleepyXm@users.noreply.github.com>
2026-01-23 18:06:23 +0000

    movement

--- app/dashboard/~/[project]/page.tsx
+++ app/dashboard/~/[project]/page.tsx
@@ -1,5 +1,16 @@
 import Terminal from "../components/terminal";
+import DatabasePage from "./database";
+import FrontendPage from "./frontend";
+import BackendPage from "./backend";
 
 export default function ProjectPage() {
-  return( <Terminal/> );
+  return(
+  <div>
+  <FrontendPage />
+  <BackendPage />
+  <DatabasePage />
+  <Terminal/>
+  </div>
+
+  );
 }
