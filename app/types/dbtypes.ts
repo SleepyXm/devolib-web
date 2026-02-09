d16c034abd7e24327fff61226c069ff7a2fc4c69
Mikayel <158308669+SleepyXm@users.noreply.github.com>
2026-02-09 16:34:24 +0000

    removals
    removed db stuff and db types, ill readd them

--- app/types/dbtypes.ts
+++
@@ -1,3 +0,0 @@
-export type Column = { name: string; type: string; expanded: boolean; linkedTableId?: number };
-export type Table = { id: number; name: string; columns: Column[]; rows: Row[] };
-export type Row = { [colName: string]: any };
