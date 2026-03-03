0b6cd05a071f7143eddad171e5822730b0c6b0f7
Mikayel <158308669+SleepyXm@users.noreply.github.com>
2026-03-03 15:52:06 +0000

    movement

--- app/components/monacoeditor.tsx
+++ app/components/monacoeditor.tsx
@@ -78,6 +78,7 @@ export default function MonacoEditor({
           fontSize: 16,
           minimap: { enabled: false },
           wordWrap: "on",
+          "renderValidationDecorations": "off",
         }}
       />
     </div>
