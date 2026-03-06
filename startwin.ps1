1fe42d5887465f7529b0c426fe50c8e2ab98beb3
Sleepy <158308669+SleepyXm@users.noreply.github.com>
2026-03-06 13:44:01 +0000

    movement
    this script is hella sick

--- startwin.ps1
+++ startwin.ps1
@@ -1,5 +1,33 @@
-Start-Job { npm run dev }
+$root = $PSScriptRoot
 
-Start-Job { cd app/backend; uvicorn main:app --reload }
+Start-Job { 
+    Set-Location $using:root
+    bun run dev 2>&1
+}
 
-Get-Job | Wait-Job
+Start-Job { 
+    Set-Location $using:root
+    docker compose up --build 2>&1
+}
+
+Start-Job { 
+    Set-Location "$using:root\app\backend"
+    uvicorn main:app --reload 2>&1
+}
+
+try {
+    while ($true) {
+        Get-Job | Receive-Job
+        Start-Sleep -Seconds 1
+    }
+}
+finally {
+    Write-Host "Stopping jobs..."
+    Get-Job | Stop-Job
+    Get-Job | Remove-Job
+
+    Get-Process -Name "bun" -ErrorAction SilentlyContinue | Stop-Process -Force
+    Get-Process -Name "uvicorn" -ErrorAction SilentlyContinue | Stop-Process -Force
+
+    docker compose down
+}
