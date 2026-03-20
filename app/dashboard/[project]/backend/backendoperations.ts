b60a074e5c7c946d8cd0f111db3835e2919fd0f6
Sleepy <158308669+SleepyXm@users.noreply.github.com>
2026-03-20 03:10:48 +0000

    nuking again
    i nuke a lot because i dont want there being duplicates, but my folder and file restructure is not happening today so...

--- app/dashboard/[project]/backend/backendoperations.ts
+++
@@ -1,6 +0,0 @@
-import { patchProjectMetadata } from "@/app/handlers/projects";
-import { Endpoint } from "../helpers/FileHandler/FileScanner";
-
-export const saveEndpoints = async (projectId: string, endpoints: Endpoint[]) => {
-  await patchProjectMetadata(projectId, { endpoints });
-};
