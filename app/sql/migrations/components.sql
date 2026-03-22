dacd2f9a8bb44610c65db1583f83a9d1bd55341f
Sleepy <158308669+SleepyXm@users.noreply.github.com>
2026-03-22 05:08:27 +0000

    movement

--- app/sql/migrations/components.sql
+++ app/sql/migrations/components.sql
@@ -6,3 +6,10 @@ ADD COLUMN utils jsonb DEFAULT '[]'::jsonb;
 
 
 ALTER TABLE users ADD COLUMN github_id TEXT UNIQUE;
+
+ALTER TABLE users ALTER COLUMN password DROP NOT NULL;
+
+ALTER TABLE users ADD COLUMN verified BOOLEAN DEFAULT FALSE;
+ALTER TABLE users ADD COLUMN verification_token TEXT;
+
+ALTER TABLE users ADD COLUMN github_access_token TEXT;
